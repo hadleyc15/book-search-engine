570eabbb76949f357a9599c2fa0e307037fb86e4
Chris Hadley <hadleyc15@yahoo.com>
2020-09-19 16:26:35 -0600

    minor adjustment to mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -26,7 +26,7 @@ export const ADD_USER = gql`
 `;
 
 export const SAVE_BOOK = gql`
-  mutation saveBook($input:savedBook!) {
+  mutation saveBook($input: savedBook!) {
     saveBook (input:$input)
         {
         _id
@@ -47,9 +47,9 @@ export const SAVE_BOOK = gql`
 `;
 
 export const REMOVE_BOOK = gql`
-  mutation removeBook($bookId: ID!) {
+  mutation removeBook($bookId: String!) {
     removeBook(bookId: $bookId) {
-      _id
+            _id
             username
             email
             bookCount
